4dff2efcb403c3777fec00a3c2c09a3137feb956
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-15 11:59:11 +1000

    Create Flatcards component.

--- component/FlatCards.tsx
+++ component/FlatCards.tsx
@@ -1,12 +1,57 @@
+import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+
 const FlatCards = () => {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default FlatCards;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 110,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#ef5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50dbb4',
+  },
+  cardThree: {
+    backgroundColor: '#5da3fa',
+  },
+});
