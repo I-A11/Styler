96de987f7f38f13ce56f2dd192ce08a8242f8f06
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-18 12:51:51 +1000

    Create image card and style it.

--- component/FancyCards.tsx
+++ component/FancyCards.tsx
@@ -11,16 +11,74 @@ const FancyCards = () => {
             uri: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQQZA8s3i80S9sJnQwPMBucnYOuPjOsPWuuWKP272agfS60vRU_0o1Vzv_6W03OySwua1OyWOjO2wlK9hVL2lzOgQ',
           }}
         />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Hawa Mahal</Text>
+          <Text style={styles.cardLabel}>Pink City </Text>
+          <Text style={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, illo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, illo.
+          </Text>
+          <Text style={styles.cardFooter}> 12 min away </Text>
+        </View>
       </View>
     </View>
   );
 };
+
 export default FancyCards;
+
 const styles = StyleSheet.create({
-  headingText: {},
-  card: {},
-  cardElevator: {},
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    margin: 8,
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 6,
+    margin: 12,
+  },
+  cardElevator: {
+    backgroundColor: '#f1eaea',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
   cardImage: {
     height: 180,
+    marginBottom: 8,
+    borderTopRightRadius: 6,
+    borderTopLeftRadius: 6,
+  },
+  cardBody: {
+    color: '#000000',
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
+  },
+  cardTitle: {
+    color: '#000000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 6,
+  },
+  cardLabel: {
+    color: '#000000',
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  cardDescription: {
+    color: '#531515',
+    fontSize: 14,
+    marginBottom: 12,
+    marginTop: 6,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#000000',
   },
 });
